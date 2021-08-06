4f0cf12e7f933e19b88fda9240f851d337bcff44
Andre <jessicaandre890@gmail.com>
2021-08-06 17:31:43 -0300

    develop

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express ();
 const path = require ('path');
 const port = 3030;
 
+const publicFolterpatha = path.resolve(__dirname, './public');
 
 app.use(express.static('public'))
 
@@ -10,4 +11,3 @@ app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views','home.html'
 app.get('/home', (req, res) => res.sendFile(path.join(__dirname, 'views','home.html')))
 
 app.listen(port,()=> console.log("Servidor corriendo en http://localhost:" + port)) 
-
